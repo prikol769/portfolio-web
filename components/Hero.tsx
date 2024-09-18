67e2d584c146f552497bb814abe8b20d64ef2aa1
Mykyta Krumalenko <prikol769@gmail.com>
2024-09-17 20:05:48 -0500

    added ParticlesComponent

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import ParticlesComponent from "./Particles";
+
+const Hero = () => {
+  return (
+    <div>
+      <ParticlesComponent id="particles" />
+    </div>
+  );
+};
+
+export default Hero;
