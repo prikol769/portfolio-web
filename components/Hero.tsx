a9ba70edb2ecd669e8d07b9a49b851f15e29942d
Mykyta Krumalenko <prikol769@gmail.com>
2024-09-20 17:54:26 -0500

    added Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,16 @@
 import ParticlesComponent from "./Particles";
+import GlassCard from "./GlassCard";
+import Avatar from "../app/assets/avatar.png";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-full">
       <ParticlesComponent />
+      <div className="h-screen max-w-[1500px] mx-auto flex justify-between items-center">
+        <GlassCard />
+        <Image src={Avatar} alt="avatar" width={600} height={600} />
+      </div>
     </div>
   );
 };
