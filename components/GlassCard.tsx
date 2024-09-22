0ec1074c1e8a013986bbb8c177e8c6dc403c89e9
Mykyta Krumalenko <prikol769@gmail.com>
2024-09-21 22:17:45 -0500

    change GlassCard from jsx to tsx

--- components/GlassCard.tsx
+++ components/GlassCard.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useRef } from "react";
 import VanillaTilt from "vanilla-tilt";
 import { Button } from "./ui/button";
-import Link from "next/link";
 
 const GlassCard = () => {
-  const glassCardRef = useRef(null);
+  const glassCardRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    VanillaTilt.init(glassCardRef.current, {
-      max: 10,
-      speed: 200,
-      glare: false,
-      "max-glare": 1,
-    });
+    if (typeof window !== "undefined" && window.innerWidth > 1024) {
+      VanillaTilt.init(glassCardRef?.current!, {
+        max: 10,
+        speed: 200,
+        glare: false,
+        "max-glare": 1,
+      });
+    }
   }, []);
 
   return (
     <div className="glassmorphic-card max-w-[680px]" ref={glassCardRef}>
-      <div className="px-6 py-8 z-1">
+      <div className="px-6 py-8 z-1 xs:px-4 xs:py-6">
         <p className="text-2xl">Welcome ! I`M</p>
-        <h1 className=" text-5xl font-semibold mt-1 mb-6">Mykyta Krumalenko</h1>
+        <h1 className="text-5xl font-semibold mt-1 mb-6 xs:text-4xl">
+          Mykyta Krumalenko
+        </h1>
         <p className="text-2xl mb-4">Full-Stack Developer</p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat
